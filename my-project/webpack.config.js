de1d65468bb3a51a89749b916aa7d877828c3cf5
zhuanghaixin <649308293@qq.com>
2020-12-27 01:01:31 +0800

    feat: webpack基础 entry/output 单入口/多入口

--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -3,10 +3,14 @@
 const path = require('path')
 
 module.exports ={
-    entry:'./src/index.js', // 1.入口文件
+    // entry:'./src/index.js', // 1.入口文件   1.1单入口  字符串 单页应用 1.2 多入口 对象 多页应用
+    entry:{
+        index:'./src/index.js',
+        search:'./src/search.js'
+    },
     output:{  // 2.出口文件
         path:path.join(__dirname,'dist'),
-        filename:'bundle.js'
+        filename:'[name].js'
     },
     mode:'production' // 3. 生产环境
 
